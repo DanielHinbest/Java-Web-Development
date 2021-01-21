a787a5d3f52dd7093fa1565bc451b74deb74d53f
Daniel Hinbest <daniel.hinbest@dcmail.ca>
2021-01-21 16:22:08 -0500

    SQL scripts and database should be set up correctly now.

--- src/webd4201/hinbestd/sql/faculty.sql
+++ src/webd4201/hinbestd/sql/faculty.sql
@@ -2,14 +2,41 @@
 -- WEBD 4201
 -- 19-January-2021
 
+-- Drops the faculty table if it exists
 DROP TABLE IF EXISTS faculty;
 
+-- Creates a new faculty table
 CREATE TABLE faculty (
-	id BIGINT REFERENCES users(id),
+	id BIGINT PRIMARY KEY REFERENCES users(id),
 	school_code CHAR(5) NOT NULL,
-	school_description VARCHAR(30) NOT NULL,
+	school_description VARCHAR(70) NOT NULL,
 	office VARCHAR(8) NOT NULL,
 	extension INT NOT NULL
+);
+
+-- Inserts 3 faculty members with the faculty IDs matching the users table
+INSERT INTO faculty VALUES (
+	100743832,
+	'BITM',
+	'School of Business, IT, & Management',
+	'C-315',
+	2710
+);
+
+INSERT INTO faculty VALUES (
+	100736293,
+	'BITM',
+	'School of Business, IT, & Management',
+	'C-122',
+	7329
+);
+
+INSERT INTO faculty VALUES (
+	100329873,
+	'SET',
+	'School of Science & Engineering Technology',
+	'C-108',
+	2302
 );
 
 SELECT * FROM faculty;
--- src/webd4201/hinbestd/sql/users.sql
+++ src/webd4201/hinbestd/sql/users.sql
@@ -2,32 +2,96 @@
 -- WEBD 4201
 -- 19-January-2021
 
+-- Creates an extension to pgcrypto for password hashing
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
---DROP TABLE IF EXISTS users;
+-- Drops the users table if it exists
+DROP TABLE IF EXISTS users;
 
+-- Creates a new users table
 CREATE TABLE users (
 	id BIGINT PRIMARY KEY,
 	password VARCHAR(40) NOT NULL,
 	first_name VARCHAR(35) NOT NULL,
 	last_name VARCHAR(50) NOT NULL,
 	email_address VARCHAR(255) NOT NULL,
-	enabled BOOLEAN DEFAULT 'false',
+	enabled BOOLEAN DEFAULT false,
 	type CHAR(2) NOT NULL,
 	enrol_date TIMESTAMP NOT NULL,
 	last_access TIMESTAMP NOT NULL
 );
 
-INSERT INTO Users VALUES(
-	100222222,
-	ENCODE(DIGEST('password','sha1'), 'hex'),
-	'Robert',
-	'McReady',
-	'bob.mcready@dcmail.ca',
-	'true',
+-- Inserts records into the users table
+INSERT INTO users VALUES (
+	100717231,
+	ENCODE(DIGEST('pass123', 'sha1'), 'hex'),
+	'Daniel',
+	'Hinbest',
+	'daniel.hinbest@rogers.com',
+	true,
 	's',
-	'2019-1-21 08:25:43',
-	'2019-1-21 11:11:11'
+	NOW(),
+	NOW()
+);
+
+INSERT INTO users VALUES (
+	100111111,
+	ENCODE(DIGEST('password', 'sha1'), 'hex'),
+	'Mike',
+	'Jones',
+	'mike.jones@dcmail.ca',
+	true,
+	's',
+	'2020-01-21 1:36',
+	'2020-01-01 1:37'
+);
+
+INSERT INTO users VALUES (
+	100843928,
+	ENCODE(DIGEST('abc123', 'sha1'), 'hex'),
+	'Spencer',
+	'O Reilly',
+	'spencer.oreilly@dcmail.ca',
+	true,
+	's',
+	'2020-01-21 1:43',
+	'2020-01-21 1:45'
+);
+
+INSERT INTO users VALUES (
+	100743832,
+	ENCODE(DIGEST('faculty', 'sha1'), 'hex'),
+	'Patrick',
+	'McCartney',
+	'patrick.mccartney@durhamcollege.ca',
+	true,
+	'f',
+	'2020-01-21 1:49',
+	'2020-01-21 1:50'
+);
+
+INSERT INTO users VALUES (
+	100736293,
+	ENCODE(DIGEST('12345678', 'sha1'), 'hex'),
+	'Jasmine',
+	'Frost',
+	'jasmine.frost@durhamcollege.ca',
+	true,
+	'f',
+	'2020-01-21 1:55',
+	'2020-01-21 1:55'
+);
+
+INSERT INTO users VALUES (
+	100329873,
+	ENCODE(DIGEST('qwerty', 'sha1'), 'hex'),
+	'Hayley',
+	'Conroy',
+	'hayley.conroy@durhamcollege.ca',
+	true,
+	'f',
+	'2020-01-21 1:59',
+	'2020-01-21 2:00'
 );
 
 SELECT * FROM users;
