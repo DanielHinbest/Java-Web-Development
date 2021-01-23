4b6641ad419a527c15d23e20da00d88e3e19c0b9
Daniel Hinbest <daniel.hinbest@dcmail.ca>
2021-01-23 01:34:37 -0500

    StudentDA mostly done, needs comments, password hashing, char data type sorted out

--- src/webd4201/hinbestd/StudentDA.java
+++ src/webd4201/hinbestd/StudentDA.java
@@ -55,24 +55,22 @@ public class StudentDA {
             System.out.println(e);
         }
     }
-    
-    public static Student retrieve(long key) throws NotFoundException
-    {
+
+    public static Student retrieve(long key) throws NotFoundException {
         aStudent = null;
-        
+
         try {
             PreparedStatement psSelect = aConnection.prepareStatement("SELECT users.id, password, first_name, "
                     + "last_name, email_address, last_access, enrol_date, enabled, type, "
                     + "program_code, program_description, year"
                     + "FROM users, students WHERE, users.id = students.id AND users.id = ?");
-            
+
             psSelect.setLong(1, key);  // If fails, try id
-            
+
             ResultSet rs = psSelect.executeQuery();
-            
+
             boolean gotIt = rs.next();
-            if (gotIt)
-            {
+            if (gotIt) {
                 id = rs.getLong("id");  // add table name if fails
                 password = rs.getString("password");
                 firstName = rs.getString("first_name");
@@ -86,12 +84,10 @@ public class StudentDA {
                 programDescription = rs.getString("program_description");
                 year = rs.getInt("year");
 //                marks = rs.getVector()                //unsure about the marks vector
-                
-                try
-                {
+
+                try {
                     aStudent = new Student(id, password, firstName, lastName, emailAddress, lastAccess, enrolDate, enabled, type, programCode, programDescription, year);
-                } catch (InvalidUserDataException iude)
-                {
+                } catch (InvalidUserDataException iude) {
                     System.out.println(iude.getMessage());
                 }
             } else {
@@ -103,9 +99,8 @@ public class StudentDA {
         }
         return aStudent;
     }
-    
-    public static boolean create(Student aStudent) throws DuplicateException
-    {
+
+    public static boolean create(Student aStudent) throws DuplicateException {
         boolean inserted = false;
 
         id = aStudent.getId();
@@ -130,7 +125,7 @@ public class StudentDA {
                 PreparedStatement psUserInsert = aConnection.prepareStatement("INSERT INTO users (id, password, "
                         + "first_name, last_name, email_address, last_access, enrol_date, enabled, type) "
                         + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
-                
+
                 psUserInsert.setLong(1, id);
                 psUserInsert.setString(2, password);
                 psUserInsert.setString(3, firstName);
@@ -141,10 +136,10 @@ public class StudentDA {
                 psUserInsert.setBoolean(8, enabled);
                 psUserInsert.setInt(9, year);
                 psUserInsert.execute();
-                
+
                 PreparedStatement psStudentInsert = aConnection.prepareStatement("INSERT INTO students "
                         + "(id, program_code, program_description, year) VALUES (?, ?, ?, ?)");
-                
+
                 psStudentInsert.setLong(1, id);
                 psStudentInsert.setString(2, programCode);
                 psStudentInsert.setString(3, programDescription);
@@ -155,5 +150,77 @@ public class StudentDA {
             }
         }
         return inserted;
+    }
+
+    public static int delete(Student aStudent) throws NotFoundException {
+        int records = 0;
+
+        id = aStudent.getId();
+
+        try {
+            PreparedStatement psUserDelete = aConnection.prepareStatement("DELETE FROM users WHERE id = ?");
+            PreparedStatement psStudentDelete = aConnection.prepareStatement("DELETE FROM students WHERE id = ?");
+
+            psUserDelete.setLong(1, id);
+            psStudentDelete.setLong(1, id);
+
+            records = psUserDelete.executeUpdate();
+            records = psUserDelete.executeUpdate();
+        } catch (NotFoundException e) {
+            throw new NotFoundException("Student with ID " + id + " does not exist.");
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return records;
+    }
+
+    public static int update(Student aStudent) throws NotFoundException {
+        int records;
+
+        id = aStudent.getId();
+        password = aStudent.getPassword();
+        firstName = aStudent.getFirstName();
+        lastName = aStudent.getLastName();
+        emailAddress = aStudent.getEmailAddress();
+        lastAccess = (Date) aStudent.getLastAccess();
+        enrolDate = (Date) aStudent.getEnrolDate();
+        enabled = aStudent.isEnabled();
+        type = aStudent.getType();
+        programCode = aStudent.getProgramCode();
+        programDescription = aStudent.getProgramDescription();
+        year = aStudent.getYear();
+//        marks = aStudent.getMarks();
+
+        try {
+            PreparedStatement psUserUpdate = aConnection.prepareStatement("UPDATE users SET password = ?, "
+                    + "first_name = ?, last_name = ?, email_address = ?, last_access = ?, enrol_date = ?, type = ?, enabled = ?"
+                    + "WHERE id = ?");
+            PreparedStatement psStudentUpdate = aConnection.prepareStatement("UPDATE students SET program_code = ?, program_description = ?, year = ?"
+                    + "WHERE id = ?");
+            
+            Student.retrieve(id);
+            
+            psUserUpdate.setString(1, password);
+            psUserUpdate.setString(2, firstName);
+            psUserUpdate.setString(3, lastName);
+            psUserUpdate.setString(4, emailAddress);
+            psUserUpdate.setDate(5, lastAccess);
+            psUserUpdate.setDate(6, enrolDate);
+//            psUserUpdate.setString(7, type); //Unsure about this one
+            psUserUpdate.setBoolean(8, enabled);
+            psUserUpdate.setLong(9, id);
+            psUserUpdate.executeUpdate();
+            
+            psStudentUpdate.setString(1, programCode);
+            psStudentUpdate.setString(2, programDescription);
+            psStudentUpdate.setString(3, year);
+            psStudentUpdate.setInt(4, year);
+            psStudentUpdate.executeUpdate();
+            
+        } catch (SQLException e) {
+            System.out.println(e);
+        } catch (NotFoundException e) {
+            throw new NotFoundException("Student with ID " + id + " does not exist.");
+        }
     }
 }
